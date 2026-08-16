f3a56daff1408591d3205e35fd0f94369ecf047d
andressa-barros <andressa.barros4194@gmail.com>
2026-08-15 22:19:04 -0300

    sua mensagem

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.querySelector(".display");
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    const valor = botao.innerText.trim();
+
+    if (valor === "C") {
+      display.innerText = "0";
+    } else if (valor === "=") {
+      try {
+        let conta = display.innerText.replace(/x/g, "*");
+        display.innerText = eval(conta);
+      } catch (e) {
+        display.innerText = "Erro";
+      }
+    } else {
+      if (display.innerText === "0") {
+        display.innerText = valor;
+      } else {
+        display.innerText += valor;
+      }
+    }
+  });
+});
